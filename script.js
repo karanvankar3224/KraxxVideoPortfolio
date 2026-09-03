5192e3ce33b5ae1ce30449e1a6b64650dfd6e1b4
Karan <90859787+karanvankar3224@users.noreply.github.com>
2026-09-03 20:42:35 +0530

    Added DZYNER

--- script.js
+++ script.js
@@ -40,11 +40,14 @@ document.addEventListener("DOMContentLoaded", () => {
   }
   //Portfolio section:-
   const portfolioItems = [
-    { title: "Promotional", category: "promotion", duration: "00:42",
+    { title: "NEXGALE", category: "motion-graphics", duration: "00:42",
       video: "Assets/Videos/Nexgale.mp4",
       gradient: "linear-gradient(135deg,#9a3412,#f97316)" },
-    { title: "Saas Animation", category: "motion-graphics", duration: "00:03",
+    {title: "Saas Animation", category: "motion-graphics", duration: "00:03",
       video: "Assets/Videos/First Saas Animation.mp4",
+      gradient: "linear-gradient(135deg,#4c1d95,#a855f7)" },
+     {title: "DZYNER", category: "motion-graphics", duration: "00:52",
+      video: "Assets/Videos/DZYNER2.mp4",
       gradient: "linear-gradient(135deg,#4c1d95,#a855f7)" },
       ];
 
